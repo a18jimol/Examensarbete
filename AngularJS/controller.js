e299dc1d772ee282df8cf9d2768da5d4aac66253
a18jimol <a18jimol@student.his.se>
2021-03-16 09:29:06 +0100

    Write out information fron geojson file
    Remove not needed items from controller.js
    
    Used get to access the geojson file.
    
    Used ng-repeat to loop through parts of the geojson file to see if I can access it

--- AngularJS/controller.js
+++ AngularJS/controller.js
@@ -1,11 +1,10 @@
-var model = {
-    framework: "Anuglar"
-};
-
 var ParkingApp = angular.module("ParkingApp", []);
 
 ParkingApp.controller("ParkingCtrl", function ($scope,$http) {
-    $scope.parking = model;
-      
+    // $scope.parking = model;
+    $http.get("Parking_Meters.geojson").then(function (parking) { // Access to the parking meters geojson-file
+        $scope.parking = parking; // adding the content of the JSON-file to $scope.parking
+        console.log($scope.parking) // console log 
+    });
       
 });
