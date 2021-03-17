610fc3c07ae1d1b8f9a5ed1acbb65c8f235ca7d6
a18jimol <a18jimol@student.his.se>
2021-03-17 10:36:46 +0100

    Load map with button
    Added button and moved map generation to the controller. added two function in controller for map and parking

--- AngularJS/controller.js
+++ AngularJS/controller.js
@@ -1,18 +1,31 @@
 var ParkingApp = angular.module("ParkingApp", []);
 
 ParkingApp.controller("ParkingCtrl", function ($scope,$http) {
-    // $scope.parking = model;
-    $http.get("Parking_Meters.geojson").then(function (parking) { // Access to the parking meters geojson-file
-        $scope.parking = parking; // adding the content of the JSON-file to $scope.parking
-        console.log($scope.parking) // console log 
-        angular.forEach($scope.parking,function(value,key){ // Loop through parking.data
-            angular.forEach(value.features,function(value2,key){ // Loop through parking.data.features
-                if( value2.properties.OBJECTID == 1000){
-                  console.log(value2.properties.LONGITUDE);
-                }
 
+    $scope.loadMap = function() {
+        console.log("hej");
+        var map = L.map('map').setView([55,10], 3);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 20,
+        attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+      }).addTo(map);
+    }
+    $scope.loadParking = function(){
+        $scope.parking = model;
+        $http.get("Parking_Meters.geojson").then(function (parking) { // Access to the parking meters geojson-file
+            $scope.parking = parking; // adding the content of the JSON-file to $scope.parking
+            console.log($scope.parking) // console log 
+            angular.forEach($scope.parking,function(value,key){ // Loop through parking.data
+                angular.forEach(value.features,function(value2,key){ // Loop through parking.data.features
+                    if( value2.properties.OBJECTID == 1000){
+                    console.log(value2.properties.LONGITUDE);
+                    }
+
+                });
             });
         });
-    });
+    }
+    
     
 });
