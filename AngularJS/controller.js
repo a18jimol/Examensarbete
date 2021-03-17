1da8fe6406fec4147bfce120074062fb6ab57dd1
a18jimol <a18jimol@student.his.se>
2021-03-17 09:56:18 +0100

    Remove HTML5 Canvas
    HTML5 Canvas won't work in a good way so will remove that

--- AngularJS/controller.js
+++ AngularJS/controller.js
@@ -5,16 +5,11 @@ ParkingApp.controller("ParkingCtrl", function ($scope,$http) {
     $http.get("Parking_Meters.geojson").then(function (parking) { // Access to the parking meters geojson-file
         $scope.parking = parking; // adding the content of the JSON-file to $scope.parking
         console.log($scope.parking) // console log 
-        var c = document.getElementById("canvas");
-        var ctx = c.getContext("2d");
         angular.forEach($scope.parking,function(value,key){ // Loop through parking.data
             angular.forEach(value.features,function(value2,key){ // Loop through parking.data.features
                 if( value2.properties.OBJECTID == 1000){
                   console.log(value2.properties.LONGITUDE);
-                  ctx.fillRect((value2.properties.LONGITUDE),(value2.properties.LATITUDE),10,10); // Use the coordinates of each parking loot to write them in the canvas
                 }
-                
-                
 
             });
         });
