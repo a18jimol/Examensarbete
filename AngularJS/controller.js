4307c96fe55b0ce066387b5bded75362c20f90a7
a18jimol <a18jimol@student.his.se>
2021-03-23 10:11:03 +0100

    Added filter limitTo

--- AngularJS/controller.js
+++ AngularJS/controller.js
@@ -1,6 +1,6 @@
 var ParkingApp = angular.module("ParkingApp", []);
 
-ParkingApp.controller("ParkingCtrl", function ($scope,$http) {
+ParkingApp.controller("ParkingCtrl", function ($scope,$http,$filter) {
     var map = L.map('map').setView([42.352,-71.072], 13); // variable for the map
     $scope.loadMap = function() {
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { // adding the map layer
@@ -11,14 +11,12 @@ ParkingApp.controller("ParkingCtrl", function ($scope,$http) {
     $scope.loadParking = function(){
         $http.get("Parking_Meters.geojson").then(function (parking) { // Access to the parking meters geojson-file
             $scope.parking = parking; // adding the content of the JSON-file to $scope.parking
-            console.log($scope.parking) // console log 
-            angular.forEach($scope.parking,function(value,key){ // Loop through parking.data
-                angular.forEach(value.features,function(value2,key){ // Loop through parking.data.features
-                    if( value2.properties.OBJECTID <= 500){ // loading the first 500
-                        var marker = L.marker([value2.properties.LATITUDE, value2.properties.LONGITUDE]).addTo(map); // making a marker with the cooridnates from the geojson file
-                    }
-
-                });
+            console.log($scope.parking); // console log 
+            
+            $scope.filteredParking = $filter('limitTo')($scope.parking.data.features, 10, 1); // filter the array data.features with a limit to the first 10
+            console.log($scope.filteredParking); 
+            angular.forEach($scope.filteredParking,function(value,key){ // Loop through parking.data      
+                var marker = L.marker([value.properties.LATITUDE, value.properties.LONGITUDE]).addTo(map); // making a marker with the cooridnates from the geojson file             
             });
         });
     }
