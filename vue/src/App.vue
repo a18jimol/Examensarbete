9a0d9c7858153d1b5d3ec384b6861a48d3f993ec
a18jimol <a18jimol@student.his.se>
2021-03-29 11:18:44 +0200

    load map and load parking
    possible to load the map and parking data in the same way as the angular app, but marker are not showing right

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,7 +1,7 @@
 <template>
 <div>
-  <button >Load Map</button>
-  <button >Load Parking</button>
+  <button  v-on:click="loadMap">Load Map</button>
+  <button  v-on:click="loadParking">Load Parking</button>
   <div id="map"></div>
   
 </div>
@@ -16,11 +16,30 @@ export default {
   data() {
     return {
       map: null,
-      parking:json.data // save the data
+      parking:json.data, // save the data
+      markerLayer : null,
+      filterParking: null
     };
   },
+  methods: {
+   
+  loadMap() {
+    L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.map);
+  },
+  loadParking() {
+      var random1 = Math.floor((Math.random()*500)+1000); // random number
+      this.markerLayer.clearLayers(); //remove all old markers
+      //console.log(json.features);
+      this.filterParking = json.features.filter(item => item.properties.OBJECTID < random1); //filter all parking
+      console.log(this.filterParking);
+      (this.filterParking).forEach((value) => {L.marker([value.properties.LATITUDE, value.properties.LONGITUDE]).addTo(this.markerLayer)}); // write the markers
+  }},
   mounted() {
     this.map = L.map("map").setView([42.352,-71.072], 13); // set view for the map
+    this.markerLayer = L.layerGroup().addTo(this.map); // layergroup 
   },
 };
 </script>
