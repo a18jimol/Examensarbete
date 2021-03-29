28e5e7e410e813c47a54a489b11db747fccd5ba8
a18jimol <a18jimol@student.his.se>
2021-03-29 10:51:18 +0200

    addad parking data and map to vue
    import leaflet and parkingdata, setview of the map.

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,10 +1,33 @@
 <template>
-  <div id="app">
-    <h1>ParkingApp with Vue</h1>
-  </div>
+<div>
+  <button >Load Map</button>
+  <button >Load Parking</button>
+  <div id="map"></div>
+  
+</div>
 </template>
+
 <script>
-  export default {
-    name: 'app'
-  }
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import json from '../../Parking_Meters.json'; // import the json data
+
+export default {
+  data() {
+    return {
+      map: null,
+      parking:json.data // save the data
+    };
+  },
+  mounted() {
+    this.map = L.map("map").setView([42.352,-71.072], 13); // set view for the map
+  },
+};
 </script>
+
+<style>
+#map{
+    height: 1000px;
+    width: 1000px;
+}
+</style>
