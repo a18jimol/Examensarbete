d051660425fac51df3d1f6ece117322a50615355
a18jimol <a18jimol@student.his.se>
2021-03-30 21:25:41 +0200

    adding markers to an layer so it can be cleared for each time

--- AngularJS/controller.js
+++ AngularJS/controller.js
@@ -8,15 +8,17 @@ angular.module("parkingApp", []).controller("ParkingCtrl", function ($scope,$htt
         attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>' // copyright to openstreetmap
       }).addTo(map);
     }
+    var layerGroup = L.layerGroup().addTo(map);
     $scope.loadParking = function(){
         $http.get("../Parking_Meters.json").then(function (parking) { // Access to the parking meters geojson-file
             $scope.parking = parking; // adding the content of the JSON-file to $scope.parking
             console.log($scope.parking); // console log 
             
             $scope.filteredParking = $filter('filter')($scope.parking.data.features, "free"); // filter out the free parkings
-            console.log($scope.filteredParking); 
+            console.log($scope.filteredParking);
+            layerGroup.clearLayers(); 
             angular.forEach($scope.filteredParking,function(value,key){ // Loop through parking.data      
-                var marker = L.marker([value.properties.LATITUDE, value.properties.LONGITUDE]).addTo(map); // making a marker with the cooridnates from the geojson file             
+                var marker = L.marker([value.properties.LATITUDE, value.properties.LONGITUDE]).addTo(layerGroup); // making a marker with the cooridnates from the geojson file             
             });
         });
     }
@@ -24,9 +26,9 @@ angular.module("parkingApp", []).controller("ParkingCtrl", function ($scope,$htt
         $http.get("../Parking_Meters.json").then(function (parking) { // Access to the parking meters geojson-file
             $scope.parking = parking; // adding the content of the JSON-file to $scope.parking
             console.log($scope.parking); // console log 
-        
+            layerGroup.clearLayers();
             angular.forEach($scope.parking.data.features,function(value,key){ // Loop through parking.data      
-                var marker = L.marker([value.properties.LATITUDE, value.properties.LONGITUDE]).addTo(map); // making a marker with the cooridnates from the geojson file             
+                var marker = L.marker([value.properties.LATITUDE, value.properties.LONGITUDE]).addTo(layerGroup); // making a marker with the cooridnates from the geojson file             
             });
         });
     }
