7b196d9ef2cc36e4fddd146947b7c74e45a229be
a18jimol <a18jimol@student.his.se>
2021-04-22 13:35:25 +0200

    Moved json-file for angularjs to the angularjs folder

--- AngularJS/controller.js
+++ AngularJS/controller.js
@@ -6,7 +6,7 @@ model.controller("ParkingCtrl", function ($scope,$http,$filter) {
     $scope.loadParking = function(){
         markerLayer.clearLayers(); 
         var start = performance.now(); // start measurement
-        $http.get("../Parking_Meters.json").then(function (parking) { // Access to the parking meters geojson-file
+        $http.get("Parking_Meters.json").then(function (parking) { // Access to the parking meters geojson-file
             $scope.parking = parking; // adding the content of the JSON-file to $scope.parking         
             $scope.filteredParking = $filter('filter')($scope.parking.data.features, "free"); // filter out the free parkings      
             angular.forEach($scope.filteredParking,function(value,key){ // Loop through parking.data      
@@ -26,7 +26,7 @@ model.controller("ParkingCtrl", function ($scope,$http,$filter) {
     $scope.loadAllParking = function(){
         markerLayer.clearLayers();
         var start = performance.now(); // start measurement
-        $http.get("../Parking_Meters.json").then(function (parking) { // Access to the parking meters geojson-file
+        $http.get("Parking_Meters.json").then(function (parking) { // Access to the parking meters geojson-file
             $scope.parking = parking; // adding the content of the JSON-file to $scope.parking
             angular.forEach($scope.parking.data.features,function(value,key){ // Loop through parking.data      
                 var marker = L.marker([value.properties.LATITUDE, value.properties.LONGITUDE]).addTo(markerLayer); // making a marker with the cooridnates from the geojson file             
