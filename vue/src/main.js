f12010d1d30ff5cfeeb5f35dade865ae76d96754
a18jimol <a18jimol@student.his.se>
2021-04-25 18:12:19 +0200

    Marker fix for Leaflet in Vue.js so markers get visible
    Marker fix for Leaflet in Vue.js, code by PTihomir (2016) https://github.com/PaulLeCam/react-leaflet/issues/255
    [Accessed 2021-04-25]
    This code is needed so the markers are retrieved from the right location.

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,7 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// Code by PTihomir(2016) see: https://github.com/PaulLeCam/react-leaflet/issues/255 
+// This code is needed so the markers are retrieved from the right location.
+import L from 'leaflet';
+delete L.Icon.Default.prototype._getIconUrl;
 
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
 
 Vue.config.productionTip = false
 
